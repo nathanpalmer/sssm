27a9f90c289fd4a214fca3ad5276c899669ec54d
Nathan Palmer <git@nathanpalmer.email>
2020-05-17 13:47:48 -0400

    Allowing the public file server in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
 
   config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
-  config.public_file_server.enabled = false
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   config.active_support.deprecation = :log
 
